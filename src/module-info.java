0c6852e3bb4ef488f6f85a14616d0d6d311310b2
Maurice Cantaert <maurice.cantaert@student.hogent.be>
2023-02-25 17:09:19 +0100

    * fix JavaFX module-info

--- src/module-info.java
+++ src/module-info.java
@@ -1,4 +1,12 @@
 module SDP2_Java_a01 {
+
+    exports domain;
+    exports repository;
+    exports gui;
+    exports application;
+    exports exceptions;
+    exports util;
+
     requires java.persistence;
     requires java.sql;
     requires java.instrument;
@@ -6,6 +14,7 @@ module SDP2_Java_a01 {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens domain;
+    opens application to javafx.fxml, javafx.graphics;
+    opens gui to javafx.fxml, javafx.graphics;
 }
