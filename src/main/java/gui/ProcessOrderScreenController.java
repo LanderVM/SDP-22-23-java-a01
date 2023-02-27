bc17269baf86d607353266d9c6ab2195cd8927e3
SanderGeuens <sander.geuens@student.hogent.be>
2023-02-27 15:51:13 +0100

    verder uitwerken gui process order

--- src/main/java/gui/ProcessOrderScreenController.java
+++ src/main/java/gui/ProcessOrderScreenController.java
@@ -1,11 +1,15 @@
+package gui;
+
 import java.util.Arrays;
 
 import domain.DomainController;
+import domain.Order;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
@@ -53,7 +57,7 @@ public class ProcessOrderScreenController extends GridPane{
 			System.out.println(e);
 		}
         
-        overviewOrderLabel.setText("Overview order:"+Integer.toString(id));
+        overviewOrderLabel.setText("Overview order: "+Integer.toString(id));
         orderTextArea.setText(dc.giveOverviewOrder(id));
         choiceBoxTransportServices.setItems(dc.getTransportServicesObservableList());
     }
@@ -66,7 +70,17 @@ public class ProcessOrderScreenController extends GridPane{
 
     @FXML
     void processOrder(ActionEvent event) {
-    	
+    	String selectionTransportService = choiceBoxTransportServices.getSelectionModel().getSelectedItem();
+    	if (selectionTransportService==null) {
+    		Alert alert = new Alert(Alert.AlertType.INFORMATION);
+    		alert.setTitle("Warning");
+    		alert.setHeaderText("You need to select a transport service in order to be able to process order");
+    		alert.showAndWait();
+    		return;
+    	}
+    	dc.processOrder(selectionTransportService, id);
+    	Stage stage = (Stage) this.getScene().getWindow();
+    	stage.setScene(parent.getScene());
     }
 
 }
