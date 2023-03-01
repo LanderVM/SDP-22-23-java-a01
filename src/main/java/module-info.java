74a4b8a19867af3de4f0ab5f30dfa581bcbe8667
Maurice Cantaert <maurice.cantaert@student.hogent.be>
2023-03-01 23:36:23 +0100

    fix module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -20,5 +20,6 @@ module SDP2_Java_a01 {
 
     opens domain;
     opens application to javafx.fxml, javafx.graphics;
+    opens gui to javafx.fxml, javafx.graphics;
     opens gui.controller to javafx.fxml, javafx.graphics;
 }
