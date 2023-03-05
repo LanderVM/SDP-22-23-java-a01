cbe33c82ab579e3ae54e0a53ed240d822869c782
SanderGeuens <sander.geuens@student.hogent.be>
2023-03-05 23:12:54 +0100

    commit uitwerken domeinlaag suppliers

---
+++ src/main/java/domain/Supplier.java
@@ -0,0 +1,11 @@
+package domain;
+
+import java.util.List;
+
+public class Supplier {
+	
+	private String name;
+	private String adress;
+	private int telephoneNumber;
+	private List<Purchaser> listPurchasers;
+}
