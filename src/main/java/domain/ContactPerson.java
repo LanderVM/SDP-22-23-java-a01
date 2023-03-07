322ddf8579e0ed22c7bfdb67fa8347285e8cca5b
Maurice Cantaert <maurice.cantaert@student.hogent.be>
2023-03-07 17:04:13 +0100

    TransportService & ContactPerson getters and setters

--- src/main/java/domain/ContactPerson.java
+++ src/main/java/domain/ContactPerson.java
@@ -2,6 +2,8 @@ package domain;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
+
 @Entity
 public class ContactPerson {
 
@@ -12,4 +14,50 @@ public class ContactPerson {
 
     private String email;
     private int phoneNumber;
+
+    public int getContactPersonId() {
+        return contactPersonId;
+    }
+
+    public void setContactPersonId(int contactPersonId) {
+        this.contactPersonId = contactPersonId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ContactPerson that = (ContactPerson) o;
+        return contactPersonId == that.contactPersonId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(contactPersonId);
+    }
+
+    @Override
+    public String toString() {
+        return "ContactPerson{" +
+                "contactPersonId=" + contactPersonId +
+                ", email='" + email + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                '}';
+    }
 }
--- src/main/java/domain/NewTransportService.java
+++ src/main/java/domain/NewTransportService.java
@@ -4,6 +4,7 @@ package domain;
 import jakarta.persistence.*;
 
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "transport_service")
@@ -19,4 +20,68 @@ public class NewTransportService {
     @OneToOne
     private TrackingCodeDetails trackingCodeDetails;
     private boolean isActive;
+
+    public int getTransportServiceId() {
+        return transportServiceId;
+    }
+
+    public void setTransportServiceId(int transportServiceId) {
+        this.transportServiceId = transportServiceId;
+    }
+
+    public List<ContactPerson> getContactPersonList() {
+        return contactPersonList;
+    }
+
+    public void setContactPersonList(List<ContactPerson> contactPersonList) {
+        this.contactPersonList = contactPersonList;
+    }
+
+    public int getTrackingCodeCharacterCount() {
+        return trackingCodeCharacterCount;
+    }
+
+    public void setTrackingCodeCharacterCount(int trackingCodeCharacterCount) {
+        this.trackingCodeCharacterCount = trackingCodeCharacterCount;
+    }
+
+    public TrackingCodeDetails getTrackingCodeDetails() {
+        return trackingCodeDetails;
+    }
+
+    public void setTrackingCodeDetails(TrackingCodeDetails trackingCodeDetails) {
+        this.trackingCodeDetails = trackingCodeDetails;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        NewTransportService that = (NewTransportService) o;
+        return transportServiceId == that.transportServiceId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(transportServiceId);
+    }
+
+    @Override
+    public String toString() {
+        return "NewTransportService{" +
+                "transportServiceId=" + transportServiceId +
+                ", contactPersonList=" + contactPersonList +
+                ", trackingCodeCharacterCount=" + trackingCodeCharacterCount +
+                ", trackingCodeDetails=" + trackingCodeDetails +
+                ", isActive=" + isActive +
+                '}';
+    }
 }
