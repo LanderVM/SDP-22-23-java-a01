f845c563bef1dc38ac25872c74be40237b13019a
Maurice Cantaert <maurice.cantaert@student.hogent.be>
2023-03-12 22:48:17 +0100

    * TransportServiceTests readability

--- src/test/java/domain/TransportServiceTests.java
+++ src/test/java/domain/TransportServiceTests.java
@@ -18,7 +18,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class TransportServiceTest {
+public class TransportServiceTests {
 
     @Mock
     EntityManager entityManager;
@@ -32,73 +32,76 @@ public class TransportServiceTest {
     private TransportServiceJPADao transportServiceJPADao;
     private TransportServiceController transportServiceController;
 
-    private TransportService transportService;
     private ContactPerson contactPerson;
     List<TransportService> transportServiceList;
 
-    @Test
-    public void getById_happyFlow() {
-        transportService = new TransportService("test", List.of(), new TrackingCodeDetails(13, false, "testprefix", VerificationType.POST_CODE), true);
+    @Nested
+    class GetTests {
+        @Test
+        public void getById_happyFlow() {
+            TransportService transportService = new TransportService("test", List.of(), new TrackingCodeDetails(13, false, "testprefix", VerificationType.POST_CODE), true);
 
-        when(entityManager.createNamedQuery("TransportService.findById", TransportService.class)).thenReturn(query);
-        when(query.setParameter(1, 1)).thenReturn(query);
-        when(query.getSingleResult()).thenReturn(transportService);
+            when(entityManager.createNamedQuery("TransportService.findById", TransportService.class)).thenReturn(query);
+            when(query.setParameter(1, 1)).thenReturn(query);
+            when(query.getSingleResult()).thenReturn(transportService);
 
-        transportServiceJPADao = new TransportServiceJPADao(entityManager);
+            transportServiceJPADao = new TransportServiceJPADao(entityManager);
 
-        assertEquals(transportService, transportServiceJPADao.get(1));
-        verify(query).setParameter(1, 1);
+            assertEquals(transportService, transportServiceJPADao.get(1));
+            verify(query).setParameter(1, 1);
+        }
+
+        @Test
+        public void getById_invalidID_throwsNoResultException() {
+            when(entityManager.createNamedQuery("TransportService.findById", TransportService.class)).thenReturn(query);
+            when(query.setParameter(1, 1)).thenReturn(query);
+            when(query.getSingleResult()).thenThrow(NoResultException.class);
+
+            transportServiceJPADao = new TransportServiceJPADao(entityManager);
+
+            assertThrows(NoResultException.class, () -> transportServiceJPADao.get(1));
+            verify(query).setParameter(1, 1);
+        }
+
+        @Test
+        public void getAll_happyFlow() {
+            transportServiceList = List.of(
+                    new TransportService("bpost", List.of(), new TrackingCodeDetails(13, true, "32", VerificationType.POST_CODE), true),
+                    new TransportService("postnl", List.of(), new TrackingCodeDetails(9, false, "postnlprefix", VerificationType.ORDER_ID), false)
+            );
+
+            when(entityManager.createNamedQuery("TransportService.findAll", TransportService.class)).thenReturn(query);
+            when(query.getResultList()).thenReturn(transportServiceList);
+
+            transportServiceJPADao = new TransportServiceJPADao(entityManager);
+            transportServiceController = new TransportServiceController(transportServiceJPADao);
+
+            assertEquals(transportServiceList.stream().map(TransportService::getName).toList(), transportServiceController.getTransportServices());
+            verify(query).getResultList();
+        }
+
+        @Test
+        public void getAllActive_happyFlow() {
+            List<TransportService> transportServiceList =
+                    List.of(new TransportService("bpost", List.of(), new TrackingCodeDetails(13, true, "32", VerificationType.POST_CODE), true),
+                            new TransportService("postnl", List.of(), new TrackingCodeDetails(9, false, "postnlprefix", VerificationType.ORDER_ID), true)
+                    );
+            when(entityManager.createNamedQuery("TransportService.findAllActive", TransportService.class)).thenReturn(query);
+            when(query.getResultList()).thenReturn(transportServiceList);
+
+            transportServiceJPADao = new TransportServiceJPADao(entityManager);
+
+            assertEquals(transportServiceList, transportServiceJPADao.getAllActive());
+            verify(query).getResultList();
+        }
     }
 
-    @Test
-    public void getById_invalidID_throwsNoResultException() {
-        when(entityManager.createNamedQuery("TransportService.findById", TransportService.class)).thenReturn(query);
-        when(query.setParameter(1, 1)).thenReturn(query);
-        when(query.getSingleResult()).thenThrow(NoResultException.class);
-
-        transportServiceJPADao = new TransportServiceJPADao(entityManager);
-
-        assertThrows(NoResultException.class, () -> transportServiceJPADao.get(1));
-        verify(query).setParameter(1, 1);
-    }
-
-    @Test
-    public void getAll_happyFlow() {
-        transportServiceList = List.of(
-                new TransportService("bpost", List.of(), new TrackingCodeDetails(13, true, "32", VerificationType.POST_CODE), true),
-                new TransportService("postnl", List.of(), new TrackingCodeDetails(9, false, "postnlprefix", VerificationType.ORDER_ID), false)
-        );
-
-        when(entityManager.createNamedQuery("TransportService.findAll", TransportService.class)).thenReturn(query);
-        when(query.getResultList()).thenReturn(transportServiceList);
-
-        transportServiceJPADao = new TransportServiceJPADao(entityManager);
-        transportServiceController = new TransportServiceController(transportServiceJPADao);
-
-        assertEquals(transportServiceList.stream().map(TransportService::getName).toList(), transportServiceController.getTransportServices());
-        verify(query).getResultList();
-    }
-
-    @Test
-    public void getAllActive_happyFlow() {
-        List<TransportService> transportServiceList =
-                List.of(new TransportService("bpost", List.of(), new TrackingCodeDetails(13, true, "32", VerificationType.POST_CODE), true),
-                        new TransportService("postnl", List.of(), new TrackingCodeDetails(9, false, "postnlprefix", VerificationType.ORDER_ID), true)
-                );
-        when(entityManager.createNamedQuery("TransportService.findAllActive", TransportService.class)).thenReturn(query);
-        when(query.getResultList()).thenReturn(transportServiceList);
-
-        transportServiceJPADao = new TransportServiceJPADao(entityManager);
-
-        assertEquals(transportServiceList, transportServiceJPADao.getAllActive());
-        verify(query).getResultList();
-    }
 
 
     // TODO: viewTransportService, deactiveTransportService, activeTransportService && all validations
 
     @Nested
-    class AddTransactionServiceTests {
+    class AddTests {
 
         @BeforeEach
         public void setupAddTransportationServiceTests() {
