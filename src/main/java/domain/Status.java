8d69538bdc750c91743af037f6c4d74874a92e37
Maurice Cantaert <maurice.cantaert@student.hogent.be>
2023-06-20 19:00:37 +0200

    fix status

--- src/main/java/domain/Status.java
+++ src/main/java/domain/Status.java
@@ -5,7 +5,7 @@ public enum Status {
 
     @Override
     public String toString() {
-        return name().substring(0, 1).toUpperCase() + name().substring(1).toLowerCase();
+        return name().substring(0, 1).toUpperCase() + name().substring(1).toLowerCase().replaceAll("_", " ");
     }
 
 }
